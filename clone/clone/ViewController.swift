04b37bea834763b7a6d5c55ab85bceddd78bf439
Pavan Kumar <pavankumarthota1994@achiever-75.local>
2018-04-05 16:40:42 +0530

    changed

--- clone/clone/ViewController.swift
+++ clone/clone/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         print("Ashok")
         print("Its Changed")
         // Do any additional setup after loading the view, typically from a nib.
     }
