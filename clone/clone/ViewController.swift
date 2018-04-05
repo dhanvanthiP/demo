4a6c401aeb6eb8aeec6ff12bf7b5514fde680071
RAJKUMAR <maroju.rajkumar@achiever-97.local>
2018-04-05 15:50:25 +0530

    first edit

---
+++ clone/clone/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  clone
+//
+//  Created by RAJKUMAR on 4/5/18.
+//  Copyright © 2018 RAJKUMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ashok")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
